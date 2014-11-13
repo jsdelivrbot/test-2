469b26e4038b172370f214cdf1f9e36d72f8f29a
Kumaran Subramanian <kums600@gmail.com>
2014-11-13 13:36:50 -0500

    test

--- ADMS-Trackpage.js
+++ ADMS-Trackpage.js
@@ -1,7 +1,7 @@
 [
 	{
 		"pageName" : "dev/bento/remote/testrig",
-		"channel"  : "remote sandbox channel kums",
+		"channel"  : "kums test channel",
 		"prop1"    : "remote value of prop 1",
         "prop11"    : "remote value of prop 11",
         "prop74"    : "remote value of prop 74",
@@ -10,11 +10,8 @@
         "prop111"    : "remote value of prop 111",
         "eVar1"    : "remote value of evar 1",
         "eVar75"    : "remote value of evar 75",
-        "eVar76"    : "remote value of evar 76",
-        "eVar74"    : "remote value of evar 74",
-        "eVar111"    : "remote value of evar 111",
-        "eVar300"    : "remote value of evar 300",
         "channel"  : "remote sandbox channel",
+        "events" : "event16",
         "contextData"   : {
             "m.contextDataKey1"    : "testcontextvalue1",
             "contextDataKey2"    : "testcontextvalue2",
