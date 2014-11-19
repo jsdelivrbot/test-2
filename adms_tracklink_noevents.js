72202383716a42a8a6a468cdfe4853d80dacda56
Kumaran Subramanian <kumaran.subramanian@viacom.com>
2014-11-19 14:03:26 -0500

    Create adms_tracklink_noevents.js

---
+++ adms_tracklink_noevents.js
@@ -0,0 +1,19 @@
+[
+{
+"linkName" : "Sample LinkName - TL",
+"channel" : "kums test channel - TL",
+"prop1" : "remote value of prop 1 - TL",
+"prop11" : "remote value of prop 11 - TL",
+"prop74" : "remote value of prop 74 - TL",
+"prop75" : "remote value of prop 75 - TL",
+"prop76" : "remote value of prop 76 - TL",
+"prop111" : "remote value of prop 111 - TL",
+"eVar1" : "remote value of evar 1 - TL",
+"eVar75" : "remote value of evar 75 - TL",
+"contextData" : {
+"m.contextDataKey1" : "testcontextvalue1 - TL",
+"contextDataKey2" : "testcontextvalue2 - TL",
+"v.contextDataKey3" : "testcontextvalue3 - TL"
+}
+}
+]
