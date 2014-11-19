ce4187f5aa9d798f54068d4637e588682dd502e1
Kumaran Subramanian <kumaran.subramanian@viacom.com>
2014-11-19 13:02:37 -0500

    Update and rename ADMS-Trackpage.js to adms_trackpage.js

---
+++ adms_trackpage.js
@@ -0,0 +1,20 @@
+[
+	{
+	"pageName" : "dev/bento/remote/testrig/TP",
+	"channel"  : "kums test channel - TP",
+	"prop1"    : "remote value of prop 1 - TP",
+        "prop11"   : "remote value of prop 11 - TP",
+        "prop74"   : "remote value of prop 74 - TP",
+        "prop75"   : "remote value of prop 75 - TP",
+        "prop76"   : "remote value of prop 76 - TP",
+        "prop111"  : "remote value of prop 111 - TP",
+        "eVar1"    : "remote value of evar 1 - TP",
+        "eVar75"   : "remote value of evar 75 - TP",
+        "events"   : "event20,event32,events33",
+        "contextData"   : {
+            "m.contextDataKey1"    : "testcontextvalue1 - TP",
+            "contextDataKey2"    : "testcontextvalue2 - TP",
+	    "v.contextDataKey3" : "testcontextvalue3 - TP"
+        }
+	}
+]
