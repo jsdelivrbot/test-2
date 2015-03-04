a1bb8a265dd318795cf63d07923fdccf022157bd
Kumaran Subramanian <kumaran.subramanian@viacom.com>
2015-03-04 11:29:00 -0500

    Update dfp_nick.js

--- dfp_nick.js
+++ dfp_nick.js
@@ -1,40 +1,36 @@
 [
-	{
-		"config" : {
-			"isCoppaCompliant" : true,
-			"audienceManagerEnabled": true
-		},
-		"adData" : {
-			"x" : 0,
-			"y"   : 0,
-			"width" : 300,
-			"height" : 250,
-			"appName" : "nick_app",
-			"sections" : "",
-			"isDFP" : true,
-			"enableCloseButton" : true,
-			"closeButtonInterval" : 5,
-			"isRefreshable" : false,
-			"refreshInterval" : 10,
-			"enableDefaultBumper" : true,
-			"enableCoppaAdBanner" : true,
-			"keyValues" : {
-				"!category" : "v1.2.3",
-				"ver"   : "v1.2.3",
-				"testmodePrefix" : "celtra_mraid"
-			},
-			"dfpData" : {
-				"metadata" : {
-					"property" : "nick",
-					"hub" : "breaking",
-					"is_main" : true
-				},
-				"mappings" : {
-					"ad-unit" : {
-						"1" : "@property"
-					}
-				}		        	
-			}
-		}
-	}
+{
+"adData" : {
+"x" : 0,
+"y" : 0,
+"width" : 300,
+"height" : 250,
+"appName" : "btg_test_app",
+"sections" : "",
+"isDFP" : true,
+"enableCloseButton" : true,
+"closeButtonInterval" : 5,
+"isRefreshable" : false,
+"refreshInterval" : 10,
+"enableDefaultBumper" : true,
+"enableCoppaAdBanner" : true,
+"keyValues" : {
+"!category" : "v1.2.3",
+"ver" : "v1.2.3",
+"testmodePrefix" : "celtra_mraid"
+},
+"dfpData" : {
+"metadata" : {
+"property" : "nick",
+"hub" : "breaking",
+"is_main" : true
+},
+"mappings" : {
+"ad-unit" : {
+"1" : "@property"
+}
+}
+}
+}
+}
 ]
